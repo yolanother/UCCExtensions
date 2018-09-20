79a9e23a6bc6348f1123eaf88382533d10b8defd
Aaron Jackson <yolanother@gmail.com>
2018-09-20 10:08:24 -0700

    Add UCCUma

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UCCExtensions
 A collection of extensions for Ultimate Character Creator
+
+
+## UMA
+This should be a nearly one click install for creating an uma
+1. Add UMA_DCS to scene
+2. Add DynamicUmaAvatar
+3. In the UMA menu click on Bone Builder
+4. Drag your dynamic avatar to the bone builder
+5. Click generate bones
+6. Open the UCC character manager
+7. Click on the Character tab
+8. Drag your dynamic avatar to the character field
+9. Choose your animator controller (I used UltimateCharacterControllerDemo)
+10. Set profile to Nolan (maybe not needed?)
+11. Hit build character
+12. Add the UCCUma script to your character
---
+++ UCCUma.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Opsive.UltimateCharacterController.Camera;
+using Opsive.UltimateCharacterController.Character;
+using Opsive.UltimateCharacterController.Input;
+using Opsive.UltimateCharacterController.Motion;
+using UMA;
+using UMA.CharacterSystem;
+using UnityEngine;
+
+public class UCCUma : MonoBehaviour {
+    [SerializeField]
+    public MonoBehaviour[] runtimeEnabledBehaviours;
+
+    private UnityInput unityInput;
+    private UltimateCharacterLocomotionHandler locomotionHandler;
+    private UltimateCharacterLocomotion locomotion;
+    private CharacterIK characterIk;
+    private Animator animator;
+    private RuntimeAnimatorController runtimeAnimationController;
+    private DynamicCharacterAvatar avatar;
+
+    // Use this for initialization
+    void Start () {
+        unityInput = GetComponent<UnityInput>();
+        locomotionHandler = GetComponent<UltimateCharacterLocomotionHandler>();
+        locomotion = GetComponent<UltimateCharacterLocomotion>();
+        characterIk = GetComponent<CharacterIK>();
+
+        animator = GetComponent<Animator>();
+        runtimeAnimationController = animator.runtimeAnimatorController;
+
+        UpdateAvatar();
+
+        SetUmaReady(false);
+    }
+
+    private void UpdateAvatar() {
+        if (null == avatar) {
+            avatar = GetComponent<DynamicCharacterAvatar>();
+            if (null != avatar) {
+                List<UMARecipeBase> recipes = new List<UMARecipeBase>();
+                for (int idx = avatar.umaAdditionalRecipes.Length - 1; idx >= 0; idx--) {
+                    UMARecipeBase recipe = avatar.umaAdditionalRecipes[idx];
+                    Debug.Log(recipe.name);
+                    if (recipe.name != "CapsuleColliderRecipe") {
+                        recipes.Add(recipe);
+                    }
+                }
+                avatar.umaAdditionalRecipes = recipes.ToArray();
+                avatar.CharacterCreated.AddListener(OnUmaReady);
+                avatar.CharacterDestroyed.AddListener(OnUmaDestroyed);
+                avatar.raceAnimationControllers.defaultAnimationController = runtimeAnimationController;
+            }
+        }
+    }
+
+    private void Update() {
+        UpdateAvatar();
+    }
+
+    private void OnDestroy() {
+        avatar.CharacterCreated.RemoveListener(OnUmaReady);
+        avatar.CharacterDestroyed.RemoveListener(OnUmaDestroyed);
+    }
+
+    private void SetUmaReady(bool ready) {
+        if (null != unityInput) {
+            unityInput.enabled = ready;
+        }
+        if (null != locomotionHandler) {
+            locomotionHandler.enabled = ready;
+        }
+        if (null != locomotion) {
+            locomotion.enabled = ready;
+        }
+        if (null != characterIk) {
+            characterIk.enabled = ready;
+        }
+
+        foreach(MonoBehaviour behaviour in runtimeEnabledBehaviours) {
+            behaviour.enabled = ready;
+        }
+    }
+
+    private void OnUmaReady(UMAData data) {
+        SetUmaReady(true);
+    }
+
+    private void OnUmaDestroyed(UMAData data) {
+        SetUmaReady(false);
+    }
+}
