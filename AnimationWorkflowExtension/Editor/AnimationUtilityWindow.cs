6b1a3d171c8bf9490edefa16800cbd65e67b7c11
Aaron Jackson <yolanother@gmail.com>
2018-10-20 11:36:52 -0700

    Add foldouts

--- AnimationWorkflowExtension/Editor/AnimationUtilityWindow.cs
+++ AnimationWorkflowExtension/Editor/AnimationUtilityWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Opsive.UltimateCharacterController.Editor.Inspectors.Utility;
 using Opsive.UltimateCharacterController.Inventory;
@@ -15,6 +16,8 @@ namespace UCCExtensions {
         private AnimatorControllerParameter[] parameters;
         private Dictionary<string, AnimatorControllerParameter> nameToParameter = new Dictionary<string, AnimatorControllerParameter>();
         private Dictionary<string, int> nameToParameterIndex = new Dictionary<string, int>();
+
+        private Dictionary<string, bool> foldouts = new Dictionary<string, bool>();
 
         static GUIStyle s_HeaderStyle;
         public static GUIStyle HeaderGUIStyle {
@@ -97,6 +100,8 @@ namespace UCCExtensions {
                     AnimatorController = controller;
                 }
             }
+
+            foldouts.Clear();
 
             UpdateParameterMappings(animatorController);
             Repaint();
@@ -212,11 +217,12 @@ namespace UCCExtensions {
                 GUILayout.BeginVertical(EditorStyles.helpBox);
                 DrawAnimatorStateTransition(transition);
                 GUILayout.EndVertical();
-                GUILayout.Space(20);
+                GUILayout.Space(8);
             }
         }
 
         private void DrawAnimatorStateTransition(AnimatorStateTransition transition) {
+
             // This is ugly. Apparently you can't modify the collection directly.
             // The only way I found that worked was to remove conditions and readd
             // on modification. There must be a better way to do it.
@@ -224,45 +230,61 @@ namespace UCCExtensions {
             List<AnimatorCondition> conditions = new List<AnimatorCondition>();
             bool modified = false;
 
+            string label = GetTransitionLabel(transition);
+            bool visibleByDefault = !foldouts.ContainsKey(label);
             GUILayout.BeginHorizontal();
             {
-                if (null != transition.destinationState) {
-                    GUILayout.Label("Transition->" + transition.destinationState.name);
-                } else if (null != transition.destinationStateMachine) {
-                    GUILayout.Label("Transition->" + transition.destinationStateMachine.name);
-                } else if (transition.isExit) {
-                    GUILayout.Label("Transition->Exit");
+                if (visibleByDefault) {
+                    GUILayout.Label(label);
+                } else {
+                    foldouts[label] = EditorGUILayout.Foldout(foldouts[label], label);
                 }
-                GUILayout.FlexibleSpace();
-                if (GUILayout.Button("+", InspectorStyles.NoPaddingButtonStyle, GUILayout.Width(16), GUILayout.Height(16))) {
-                    AnimatorCondition condition = new AnimatorCondition();
-                    condition.parameter = parameterNames[0];
-                    conditions.Add(condition);
-                    modified = true;
+                if (visibleByDefault || foldouts[label]) {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("+", InspectorStyles.NoPaddingButtonStyle, GUILayout.Width(16), GUILayout.Height(16))) {
+                        AnimatorCondition condition = new AnimatorCondition();
+                        condition.parameter = parameterNames[0];
+                        conditions.Add(condition);
+                        modified = true;
+                    }
                 }
             }
             GUILayout.EndHorizontal();
 
-            for (int i = 0; i < transition.conditions.Length; i++) {
-                var condition = transition.conditions[i];
-                bool remove;
-                if(DrawAnimatorCondition(ref condition, out remove)) {
-                    if (!remove) conditions.Add(condition);
-                    modified = true;
-                } else {
-                    conditions.Add(transition.conditions[i]);
-                }
-            }
-
-            if (modified) {
-                while (transition.conditions.Length > 0) {
-                    transition.RemoveCondition(transition.conditions[0]);
+            if (visibleByDefault || foldouts[label]) {
+                for (int i = 0; i < transition.conditions.Length; i++) {
+                    var condition = transition.conditions[i];
+                    bool remove;
+                    if (DrawAnimatorCondition(ref condition, out remove)) {
+                        if (!remove) conditions.Add(condition);
+                        modified = true;
+                    } else {
+                        conditions.Add(transition.conditions[i]);
+                    }
                 }
 
-                foreach (AnimatorCondition condition in conditions) {
-                    transition.AddCondition(condition.mode, condition.threshold, condition.parameter);
+                if (modified) {
+                    while (transition.conditions.Length > 0) {
+                        transition.RemoveCondition(transition.conditions[0]);
+                    }
+
+                    foreach (AnimatorCondition condition in conditions) {
+                        transition.AddCondition(condition.mode, condition.threshold, condition.parameter);
+                    }
                 }
             }
+        }
+
+        private string GetTransitionLabel(AnimatorStateTransition transition, AnimatorState state = null) {
+            string label = (null == state ? "Transition" : state.name) + " -> ";
+            if (null != transition.destinationState) {
+                label += transition.destinationState.name;
+            } else if (null != transition.destinationStateMachine) {
+                label += transition.destinationStateMachine.name;
+            } else if (transition.isExit) {
+                label += "Exit";
+            }
+            return label;
         }
 
         private void OnGUI() {
@@ -273,9 +295,27 @@ namespace UCCExtensions {
                 scroll = GUILayout.BeginScrollView(scroll); {
                     stateCollection = (AnimationStateCollection)EditorGUILayout.ObjectField(stateCollection, typeof(AnimationStateCollection));
                     if (Selection.activeObject is AnimatorStateTransition) {
-                        DrawAnimatorStateTransition(Selection.activeObject as AnimatorStateTransition);
+                        var transition = Selection.activeObject as AnimatorStateTransition;
+                        if (foldouts.Count == 0) {
+                            int index = 0;
+                            foreach (AnimatorCondition condition in transition.conditions) {
+                                foldouts[transition.name + index++] = false;
+                            }
+                        }
+                        DrawAnimatorStateTransition(transition);
                     } else if (Selection.activeObject is AnimatorState) {
-                        DrawAnimatorState(Selection.activeObject as AnimatorState);
+                        AnimatorState animatorState = Selection.activeObject as AnimatorState;
+                        if (foldouts.Count == 0) {
+                            foreach (AnimatorStateTransition transition in animatorState.transitions) {
+                                string name = GetTransitionLabel(transition);
+                                foldouts[name] = false;
+                                int index = 0;
+                                foreach(AnimatorCondition condition in transition.conditions) {
+                                    foldouts[name + index++] = false;
+                                }
+                            }
+                        }
+                        DrawAnimatorState(animatorState);
                     }
                 } GUILayout.EndScrollView();
             }
